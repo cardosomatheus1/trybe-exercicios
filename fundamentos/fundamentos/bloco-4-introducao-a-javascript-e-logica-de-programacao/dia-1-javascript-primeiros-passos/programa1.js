08c10717b983d2e527334a2c40e11b1e60bc6a9c
Matheus <matheus-cardoso_@hotmail.com>
2022-03-08 13:38:51 -0300

    até exersisio 5

---
+++ fundamentos/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa1.js
@@ -0,0 +1,79 @@
+//programa 1
+
+let a = 6;
+let b = 2;
+
+let adição = a+b;
+console.log(adição);
+let subtração = a-b;
+console.log(subtração);
+let multiplicação = a*b;
+console.log(multiplicação);
+let divisão = a/b;
+console.log(divisão);
+let modulo = a%b;
+console.log(modulo);
+
+//programa 2
+
+const x = 4
+const y = 9
+
+if (x>y){
+    console.log (x)
+}
+else if (y>x){
+    console.log(y)
+}
+else(
+    console.log(x,y)
+)
+//programa 3
+
+const q = 440
+const w = 440
+const e = 440
+
+if ( q>w && q>e)
+{
+    console.log(q);
+}
+else if (w>q && w>e){
+console.log(w);
+}
+else if ( e>w && e>q){
+    console.log(e);
+}
+else if (q == w && q!= e){
+    console.log(q,w);
+}
+else if (q == e && q!=w){
+console.log(q,e);
+}
+else if (e == w && e!= q){
+    console.log(e,w);
+}
+else if (e == w && e ==q){
+    console.log(q,e,w);
+}
+ 
+//programa 4
+
+const v = 0
+
+if (v<0){
+    console.log("negative");
+}
+else if (v>0){
+    console.log("positive");
+}
+else{
+    console.log("zero")
+}
+
+//programa 5
+
+const r = 60
+const t = 60
+const u = 60
+
